11036cf6f098dd6ca09469743a8da15901884603
Cedric Lemaitre <c.lemaitre58@gmail.com>
2020-04-04 18:35:33 +0200

    problem1-2 validation

--- QtProject/labs1/main.cpp
+++ QtProject/labs1/main.cpp
@@ -15,12 +15,26 @@ int main()
     //---------------------------------
     int a = 0;
     int b = 0;
-    std::cout << "Give first value" << std::endl;
-    std::cin >> a;
-    std::cout << "Give second value" << std::endl;
-    std::cin >> b;
-    std::cout << "Value for a before swaping\t" << a << "Value for b before swaping\t" << b << std::endl;
-    problem1::swap(a, b);
-    std::cout << "Value for a after swaping\t" << a << "Value for b after swaping\t" << b << std::endl;
+//    std::cout << "Give first value" << std::endl;
+//    std::cin >> a;
+//    std::cout << "Give second value" << std::endl;
+//    std::cin >> b;
+//    std::cout << "Value for a before swaping\t" << a << "\tValue for b before swaping\t" << b << std::endl;
+//    problem1::swap(a, b);
+//    std::cout << "Value for a after swaping\t" << a << "\tValue for b after swaping\t" << b << std::endl;
+    // test same signe function
+    // all négatif
+    a = -1;
+    b = -1;
+    std::cout << "a : \t" << a << "\tb : \t" << b << "\t result : \t" << problem1::same_sign(a, b) << std::endl;
+    a = 1;
+    b = -1;
+    std::cout << "a : \t" << a << "\tb : \t" << b << "\t result : \t" << problem1::same_sign(a, b) << std::endl;
+    a = -1;
+    b = 1;
+    std::cout << "a : \t" << a << "\tb : \t" << b << "\t result : \t" << problem1::same_sign(a, b) << std::endl;
+    a = 1;
+    b = 1;
+    std::cout << "a : \t" << a << "\tb : \t" << b << "\t result : \t" << problem1::same_sign(a, b) << std::endl;
     return 0;
 }
